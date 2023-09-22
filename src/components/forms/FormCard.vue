ace2fdf67526d8feba2e6b572cfb8abf1fa56070
[Claudiu-Whitecat] <[Claudiu@whitecat.ro]>
2023-09-22 21:07:21 +0300

    First commit

---
+++ src/components/forms/FormCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Claudiu</h1>
+</template>
+<script>
+export default {
+  computed: {},
+};
+</script>
+<style scoped>
+h1 {
+  color: green;
+  font-size: 5rem;
+  text-decoration: underline;
+}
+</style>
